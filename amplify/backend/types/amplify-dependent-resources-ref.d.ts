173fa047f13c5e68064b3c84e130d46ed0b74565
lxmh <lxmhoang@gmail.com>
2024-03-05 15:29:11 +0700

    tutorial graphQL and fix package.lock

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "reactjsf319": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "reactjsf319": {
       "AppClientID": "string",
